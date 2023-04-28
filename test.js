7bd92823fa8a46f163dd90d7ed5348fec07228a0
Auric Vente <mprops@protonmail.com>
2023-04-27 23:21:26 -0600

    first commit

---
+++ test.js
@@ -0,0 +1,66 @@
+const App = {}
+
+App.reply = function (abc) {
+  //
+}
+
+App.shout = function () {
+  //
+}
+
+App.talk = function (a, b) {
+  //
+}
+
+App.sleep = function (a = 123, b = "home") {
+  //
+}
+
+let a = "This is a string"
+
+let list = [
+  "abc",
+  "def",
+]
+
+let empty = ""
+
+if (a === "123") {
+  //
+} else if (a === "444") {
+  //
+} else {
+  //
+}
+
+
+
+
+
+if (a) {
+  if (a) {
+    if (a) {
+      if (a) {
+        if (a === "123") {
+          try {
+            //
+          } catch (err) {
+            //
+          }
+        } else if (a === "444") {
+          //
+        } else {
+          //
+        }
+      }
+    }
+  }
+}
+
+try {
+  //
+} catch (err) {
+  //
+}
+
+console.log(".chat_quote_profilepic", "something")
